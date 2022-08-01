7d345866c135bad27a1956f4184cf0c81b2b87f7
iforgotmysocks <iforgotmysocks@users.noreply.github.com>
2022-08-01 22:32:02 +0200

    init

---
+++ Source/ProjectileOffset/ProjectileOffset/PlaceWorker_ShowTurretRadiusWithOffset.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace ProjectileOffset
+{
+    class PlaceWorker_ShowTurretRadiusWithOffset : PlaceWorker
+    {
+		public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null, Thing thing = null)
+		{
+			VerbProperties verbProperties = ((ThingDef)checkingDef).building.turretGunDef.Verbs.Find((VerbProperties v) => v.verbClass == typeof(Verb_LaunchWithOffset));
+			if (verbProperties.range > 0f)
+			{
+				GenDraw.DrawRadiusRing(loc, verbProperties.range);
+			}
+			if (verbProperties.minRange > 0f)
+			{
+				GenDraw.DrawRadiusRing(loc, verbProperties.minRange);
+			}
+			return true;
+		}
+	}
+}
